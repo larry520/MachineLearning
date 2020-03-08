04e0f9db498bcb06a1b5382bbfa69cc2440de06b
larry.chen <larry.chen@cygia.com>
2020-03-08 13:46:45 +0800

    update

--- minist/practice_1.py
+++ minist/practice_1.py
@@ -2,17 +2,68 @@
 
 from minist.unpack import *
 import numpy as np
+from sklearn.model_selection import cross_val_predict, cross_val_score
+import threading
+from sklearn.metrics import confusion_matrix
+
 
 train_data = load_train_images()
 train_label = load_train_labels()
-test_data  = load_test_images()
-test_label = load_test_labels()
+# test_data  = load_test_images()
+# test_label = load_test_labels()
 
 # X_train = train_data.reshape(-1,784)
 
 shuffle_index = np.random.permutation(60000)
-X_train = train_data[shuffle_index]
+X_train = train_data[shuffle_index].reshape(-1,784)
 X_label = train_label[shuffle_index]
+
+X_train = X_train[:10000]
+X_label = X_label[:10000]
+
 
 from sklearn.neighbors import KNeighborsClassifier
 knn_clf = KNeighborsClassifier()
+# knn_clf.fit(X_train,X_label)
+X_trian_knn_score = cross_val_score(knn_clf,X_train,X_label,cv=3)
+
+X_trian_knn_pred = cross_val_predict(knn_clf,X_train,X_label,cv=3)
+conf_mx = confusion_matrix(X_label, X_trian_knn_pred)
+plt.matshow(conf_mx)  # 混淆矩阵图，彩色的
+# 计算错误率
+np.fill_diagonal(conf_mx,0)
+row_sums = conf_mx.sum(axis=1, keepdims=True)
+norm_conf_mx = conf_mx/row_sums  # 每行除以该类别个数，得到相应错误识别的概率
+plt.matshow(norm_conf_mx, cmap=plt.cm.get_cmap("gray"))
+
+cl_a,cl_b = 1,7
+X_aa = X_train[(X_label==cl_a) & (X_trian_knn_pred==cl_a)]
+X_ab = X_train[(X_label==cl_a) & (X_trian_knn_pred==cl_b)]
+X_bb = X_train[(X_label==cl_b) & (X_trian_knn_pred==cl_b)]
+X_ba = X_train[(X_label==cl_b) & (X_trian_knn_pred==cl_a)]
+
+def plot_digits(instances,imapges_per_row=5,**options):
+    size = 28
+    imapges_per_row = min(len(instances), imapges_per_row)
+    # images = [instance.reshape(size, size) for instance in instances] #对每行数据还原为图片
+    images = instances.reshape(-1,size,size)
+    # rows = (len(images) -1)//imapges_per_row + 1
+    rows = (images.shape[0] -1)//imapges_per_row + 1
+    n_empty = rows*imapges_per_row - len(images)
+    images = np.concatenate((images,np.zeros((n_empty,size,size))),axis=0)
+    print(images.shape)
+    row_images = []
+    for row in range(rows):
+        rimages = images[row*imapges_per_row:(row+1)*imapges_per_row]
+        row_images.append(np.concatenate(rimages, axis=1))
+    mat_images = np.concatenate(row_images,axis=0)
+    plt.imshow(mat_images,cmap=plt.cm.get_cmap("binary"),**options)
+    plt.axis("off")
+
+plt.figure(figsize=(8,8))
+plt.subplot(221); plot_digits(X_aa[:25],5)
+plt.subplot(222); plot_digits(X_ab[:25],5)
+plt.subplot(223); plot_digits(X_ba[:25],5)
+plt.subplot(224); plot_digits(X_bb[:25],5)
+
+
--- python skill.py
+++ python skill.py
@@ -1,26 +1,28 @@
 # -*- encoding:utf-8 -*-
 import json
-
+import numpy as np
 # region  Json 序列化与反序列化
-d = '{"name":"小明","age":18,"skill":["开飞机","铁锅炖大鹅"]}'
-d_json = json.loads(d)  # 可处理复杂的字典嵌套
-d_eval = eval(d)  # 系统自带
+def json_serialize():
+    d = '{"name":"小明","age":18,"skill":["开飞机","铁锅炖大鹅"]}'
+    d_json = json.loads(d)  # 可处理复杂的字典嵌套
+    d_eval = eval(d)  # 系统自带
 
-print(d_eval,d_json)
+    print(d_eval,d_json)
 
-class Student(object):
-    def __init__(self, name, age, score):
-        self.name = name
-        self.age = age
-        self.score = score
+    class Student(object):
+        def __init__(self, name, age, score):
+            self.name = name
+            self.age = age
+            self.score = score
 
-s = Student('Bob', 20, 88)
-# default用于对复杂结构定义序列化时转化规则
-# 详情看 https://www.liaoxuefeng.com/wiki/1016959663602400/1017624706151424
-s_json = json.dumps(s, default=lambda obj:obj.__dict__)
-print(type(s_json),s_json)
-s_recall = json.loads(s_json)
-print(type(s_recall), s_recall)
+    s = Student('Bob', 20, 88)
+    # default用于对复杂结构定义序列化时转化规则
+    # 详情看 https://www.liaoxuefeng.com/wiki/1016959663602400/1017624706151424
+    s_json = json.dumps(s, default=lambda obj:obj.__dict__)
+    print(type(s_json),s_json)
+    s_recall = json.loads(s_json)
+    print(type(s_recall), s_recall)
+# json_serialize()
 # endregion
 
 # region 二进制流 struck module
@@ -48,8 +50,38 @@ def demo1():
     t = t_bin.decode("utf-8")
     print(t_bin,"t_bin.type:",type(t_bin),"<==>",t,"t.type:",type(t))
 
-demo1()
+# demo1()
 # endregion
 
+# region numpy array 合并、连接、降维
 
+# np.concatenate 将列表进行连接，多个列表则首项连接，单个列表则降维连接
+    """
+    numpy.flatten()/numpy.ravel() 将多维数组降位一维 默认行优先，x.ravel("F") 则列优先
+    两者的区别在于返回拷贝（copy）还是返回视图（view），
+    numpy.flatten()返回一份拷贝，对拷贝所做的修改不会影响（reflects）原始矩阵
+    而numpy.ravel()返回的是视图（view），会影响（reflects）原始矩阵。
+    https://blog.csdn.net/liuweiyuxiang/article/details/78220080
+    """
+
+import numpy as np
+import pandas as pd
+def array_con():
+    a = np.zeros((3,3))
+    b = np.ones((3,3))
+    c = np.concatenate((a,b),axis=0)  # 行连接，列数不变 np.r_([a,b])
+    d = np.concatenate((a,b),axis=1)  # 列连接，行数不变 np.c_([a,b])
+    n_zero = np.zeros((5,2,3,3))
+    n_one =np.ones((2,2,3,3))
+    cmb = np.concatenate((n_one,n_zero),axis=0)
+    cmb_one = np.concatenate(n_one)   # shape [2.2.3.3] --> [4,3,3]
+    a_ = pd.DataFrame(a)
+    b_ = pd.DataFrame(b)
+    c_ = pd.concat((a_,b_),axis=0)  # 行连接，列数不变
+    d_ = pd.concat((a_,b_),axis=1)  # 列连接，行数不变
+    e_ = pd.merge(a_,b_,how="outer") # 上下联结
+    e = e_[1].tolist()
+array_con()
+
+# endregion、、
 
